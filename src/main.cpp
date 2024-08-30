803d286c7efd3ca0e8ca8f31f8a2af32fdc33ad0
Rafael Niebles <rnieblesealo@gmail.com>
2024-08-30 15:10:53 -0400

    mess w rot, alpha & color mod

--- src/main.cpp
+++ src/main.cpp
@@ -13,30 +13,20 @@
 #include <stdio.h>
 #include <string.h>
 
-const int SCREEN_WIDTH = 512;
-const int SCREEN_HEIGHT = 448;
-const int GLOB_SCALE = 5;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 800;
+const int GLOB_SCALE = 8;
 
-SDL_Window *window = NULL; // render target
+SDL_Window *window = NULL;         // render target
 SDL_Surface *screenSurface = NULL; // screen surface
-SDL_Renderer *renderer = NULL; // work with textures; hardware blitting
+SDL_Renderer *renderer = NULL;     // work with textures; hardware blitting
 
 // we can initialize rects like this because they're structs
 // structs are like arrays with unevenly sized elements
-SDL_Rect screenRect = {
-  0, 
-  0, 
-  SCREEN_WIDTH, 
-  SCREEN_HEIGHT
-};
+SDL_Rect screenRect = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 
 // inputs; used to check iskeydown
-typedef enum Inputs {
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
-} Inputs;
+typedef enum Inputs { UP, DOWN, LEFT, RIGHT } Inputs;
 
 const int KEY_COUNT = 4;
 bool KEYS[KEY_COUNT];
@@ -49,14 +39,21 @@ bool Init() {
   }
 
   // make window
-  window = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  window =
+      SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                       SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
   if (window == NULL) {
     printf("Window creation failed: %s\n", SDL_GetError());
     return false;
   }
 
   // make renderer for window
-  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  renderer = SDL_CreateRenderer(
+      window, -1,
+      SDL_RENDERER_ACCELERATED); // we can | SDL_RENDERER_PRESENTVSYNC to enable
+                                 // that, but don't; already have target fps
+                                 // system
+
   if (renderer == NULL) {
     printf("Could not create renderer :%s\n", SDL_GetError());
     return false;
@@ -75,7 +72,7 @@ bool Init() {
 
   // get window surface
   screenSurface = SDL_GetWindowSurface(window);
-  
+
   // set keys
   for (int i = 0; i < KEY_COUNT; ++i)
     KEYS[i] = false;
@@ -86,40 +83,39 @@ bool Init() {
 // texture wrapper
 class LTexture {
 public:
-  LTexture(){
+  LTexture() {
     texture = NULL;
     width = 0;
     height = 0;
   }
 
-  ~LTexture(){
-    Free();  
-  }
+  ~LTexture() { Free(); }
 
-  bool LoadFromFile(const char* path){
+  bool LoadFromFile(const char *path) {
     // remove pre-existing texture
     Free();
 
     // load new one
-    SDL_Surface* lSurf = IMG_Load(path);
+    SDL_Surface *lSurf = IMG_Load(path);
 
-    if (lSurf == NULL){
+    if (lSurf == NULL) {
       printf("Unable to load image: %s\n", SDL_GetError());
       return false;
     }
 
     // set color key to black
-    SDL_SetColorKey(lSurf, SDL_TRUE, SDL_MapRGB(lSurf->format, 0x00, 0x00, 0x00));
-   
+    SDL_SetColorKey(lSurf, SDL_TRUE,
+                    SDL_MapRGB(lSurf->format, 0x00, 0x00, 0x00));
+
     // make texture w/color key
     SDL_Texture *nTexture = SDL_CreateTextureFromSurface(renderer, lSurf);
- 
-    if (nTexture == NULL){
+
+    if (nTexture == NULL) {
       printf("Could not create texture: %s\n", SDL_GetError());
       return false;
     }
 
-    width = lSurf->w; 
+    width = lSurf->w;
     height = lSurf->h;
 
     // get rid of interim surface
@@ -127,15 +123,15 @@ public:
 
     // set new texture
     texture = nTexture;
-   
+
     // set scale to global
     SetScale(GLOB_SCALE);
 
     return true;
   }
 
-  void Free(){
-    if (texture != NULL){
+  void Free() {
+    if (texture != NULL) {
       SDL_DestroyTexture(texture);
       texture = NULL;
       width = 0;
@@ -143,18 +139,26 @@ public:
     }
   }
 
-  void Render(int x, int y, SDL_Rect *clip = NULL){
+  void SetBlendMode(SDL_BlendMode blendMode) {
+    // set blend mode
+    SDL_SetTextureBlendMode(texture, blendMode);
+  }
+
+  // modulation ~ multiplication!
+
+  void ModColor(Uint8 r, Uint8 g, Uint8 b) {
+    SDL_SetTextureColorMod(texture, r, g, b);
+  }
+
+  void ModAlpha(Uint8 a) { SDL_SetTextureAlphaMod(texture, a); }
+
+  void Render(int x, int y, SDL_Rect *clip = NULL) {
     // set render space on screen
     // sprite will be stretched to match
-    SDL_Rect renderDest = {
-      x,
-      y,
-      width * scale,
-      height * scale 
-    };
-    
+    SDL_Rect renderDest = {x, y, width * scale, height * scale};
+
     // give dest rect the dimensions of the src rect
-    if (clip != NULL){
+    if (clip != NULL) {
       renderDest.w = clip->w * scale;
       renderDest.h = clip->h * scale;
     }
@@ -164,32 +168,35 @@ public:
     SDL_RenderCopy(renderer, texture, clip, &renderDest);
   }
 
-  void RenderFill(){
+  void RenderRotated(int x, int y, SDL_Rect *clip, double angle,
+                     SDL_Point *center, SDL_RendererFlip flip) {
+    SDL_Rect renderDest = {x, y, width * scale, height * scale};
+
+    // careful! this isn't given a value by default
+    if (clip != NULL) {
+      renderDest.w = clip->w * scale;
+      renderDest.h = clip->h * scale;
+    }
+
+    // pass sprite through rotation
+    SDL_RenderCopyEx(renderer, texture, clip, &renderDest, angle, center, flip);
+  }
+
+  void RenderFill() {
     // stretch to fill screen
-    SDL_Rect renderDest = {
-      0,
-      0,
-      SCREEN_WIDTH,
-      SCREEN_HEIGHT
-    };
+    SDL_Rect renderDest = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 
-    SDL_RenderCopy(renderer, texture, NULL, &renderDest); 
+    SDL_RenderCopy(renderer, texture, NULL, &renderDest);
   }
 
-  int GetWidth(){
-    return width;
-  }
+  int GetWidth() { return width; }
 
-  int GetHeight(){
-    return height;
-  }
+  int GetHeight() { return height; }
 
-  void SetScale(int nScale){
-    scale = nScale;
-  }
+  void SetScale(int nScale) { scale = nScale; }
 
 private:
-  SDL_Texture* texture;
+  SDL_Texture *texture;
 
   int width;
   int height;
@@ -199,12 +206,12 @@ private:
 auto lastUpdateTime = std::chrono::high_resolution_clock::now();
 float dt = 0;
 
-class LSprite{
+class LSprite {
 public:
-  LSprite(LTexture* spriteSheet, SDL_Rect *spriteClips, int nFrames){
-    this->spriteSheet = spriteSheet; 
+  LSprite(LTexture *spriteSheet, SDL_Rect *spriteClips, int nFrames) {
+    this->spriteSheet = spriteSheet;
     this->spriteClips = spriteClips;
-    
+
     this->nFrames = nFrames;
 
     currentFrame = 0;
@@ -212,10 +219,10 @@ public:
     fps = 8;
   }
 
-  void Render(int x, int y){
-    fTimer += dt;        
-    
-    if (fTimer > (1.0 / fps)){
+  void Render(int x, int y) {
+    fTimer += dt;
+
+    if (fTimer > (1.0 / fps)) {
       if (currentFrame + 1 == nFrames)
         currentFrame = 0;
       else
@@ -224,7 +231,12 @@ public:
       fTimer = 0;
     }
 
+    // draw normal
     spriteSheet->Render(x, y, &spriteClips[currentFrame]);
+
+    // draw rotated!
+    // spriteSheet->RenderRotated(x, y, &spriteClips[currentFrame], 90, NULL,
+    // SDL_FLIP_NONE);
   }
 
 private:
@@ -232,60 +244,46 @@ private:
   SDL_Rect *spriteClips;
 
   int currentFrame;
-  int nFrames; 
+  int nFrames;
   int fps;
   float fTimer;
 };
 
 LTexture tBackground;
 
-// character sprite 
+// character sprite
 LTexture tSpriteSheet;
-SDL_Rect spriteClips[]= {
-  // frame 1
-  {
-    0,
-    0,
-    16,
-    16
-  },
-  // frame 2
-  {
-    0,
-    16,
-    16,
-    16
-  }
-};
+SDL_Rect spriteClips[] = {
+    // frame 1
+    {0, 0, 16, 16},
+    // frame 2
+    {0, 16, 16, 16}};
 
 LSprite characterSprite(&tSpriteSheet, spriteClips, 2);
 
-bool LoadMedia(){
+bool LoadMedia() {
   bool success = true;
 
-  if (!tBackground.LoadFromFile("../assets/grass.png")){
+  if (!tBackground.LoadFromFile("../assets/grass.png")) {
     printf("Could not load image: %s\n", SDL_GetError());
     success = false;
   }
 
-  if (!tSpriteSheet.LoadFromFile("../assets/ness.png")){
+  // this makes grass red
+  // tBackground.ModColor(255, 0, 255);
+
+  if (!tSpriteSheet.LoadFromFile("../assets/ness.png")) {
     printf("Could not load image: %s\n", SDL_GetError());
     success = false;
   }
-  
-  spriteClips[0] = {
-    0,
-    0,
-    16,
-    16
-  };
 
-  spriteClips[1] = {
-    0,
-    16,
-    16,
-    16
-  };
+  // this makes the char abt. half as opaque
+  // tSpriteSheet.SetBlendMode(SDL_BLENDMODE_BLEND);
+  // tSpriteSheet.ModAlpha(125);
+
+  spriteClips[0] = {0, 0, 16, 16};
+
+  spriteClips[1] = {0, 16, 16, 16};
 
   return success;
 }
@@ -307,7 +305,7 @@ void Close() {
 }
 
 // limits fps; adapt to set to a framerate
-float targetFps = 60; 
+float targetFps = 120;
 
 int charPosX = 0;
 int charPosY = 0;
@@ -324,11 +322,13 @@ int main(int argc, char *argv[]) {
   SDL_Event e;
   bool quit = false;
   while (!quit) {
-    // get curr.time for dt calculation 
+    // get curr.time for dt calculation
     auto currentTime = std::chrono::high_resolution_clock::now();
 
-    // compute dt 
-    dt = std::chrono::duration<float, std::chrono::seconds::period>(currentTime - lastUpdateTime).count();
+    // compute dt
+    dt = std::chrono::duration<float, std::chrono::seconds::period>(
+             currentTime - lastUpdateTime)
+             .count();
 
     // if dt does not exceed frame duration, move on
     if (dt < 1 / targetFps)
@@ -361,7 +361,6 @@ int main(int argc, char *argv[]) {
         }
       }
 
-
       // keydown event
       else if (e.type == SDL_KEYUP) {
         switch (e.key.keysym.sym) {
@@ -388,7 +387,7 @@ int main(int argc, char *argv[]) {
 
     if (KEYS[DOWN])
       charPosY += charSpeed;
-    
+
     if (KEYS[LEFT])
       charPosX -= charSpeed;
 
@@ -397,7 +396,7 @@ int main(int argc, char *argv[]) {
 
     // clear screen
     SDL_RenderClear(renderer);
-    
+
     // draw
     tBackground.RenderFill();
     characterSprite.Render(charPosX, charPosY);
